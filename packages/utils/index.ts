038d0b290c35760241afe1ef26a5a4676b108c18
weiyafei <2553241022@qq.com>
2022-07-09 16:38:42 +0800

    📦 NEW: workspace

---
+++ packages/utils/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2022-07-09 15:19:49
+ * @LastEditTime: 2022-07-09 16:35:34
+ * @LastEditors: wsy
+ */
+export function add(a: number, b: number): number {
+  return a + b;
+}
