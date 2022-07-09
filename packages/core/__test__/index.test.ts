f5459a7457a7181f44dfa99a90fb5188d281c679
weiyafei <2553241022@qq.com>
2022-07-09 17:46:48 +0800

    vitest

---
+++ packages/core/__test__/index.test.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2022-07-09 17:30:19
+ * @LastEditTime: 2022-07-09 17:30:22
+ * @LastEditors: wsy
+ */
+describe("cac init", () => {
+  it("should init", () => {
+    expect(1).toBe(1);
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vitest/config'
-import AutoImport from 'unplugin-auto-import/vite'
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2022-07-09 17:30:00
+ * @LastEditTime: 2022-07-09 17:45:44
+ * @LastEditors: wsy
+ */
+import { defineConfig } from "vitest/config";
+import AutoImport from "unplugin-auto-import/vite";
 
 export default defineConfig({
   plugins: [
     AutoImport({
-      imports: ['vitest'],
+      imports: ["vitest"],
     }),
   ],
   test: {
-    includeSource: ['src/*'],
+    includeSource: ["packages/*"],
   },
-})
+});
